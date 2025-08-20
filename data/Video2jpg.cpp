a16df649c3bd408dd57a59a4f4f81e10902421d2
Weiming Zhao <borisdimitri2021@gmail.com>
2025-08-20 10:26:20 +0800

    Training Data Image Generation Usable

---
+++ data/Video2jpg.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <fmt/format.h>
+#include <string>
+using namespace cv;
+
+std::string ObjectName = "mouse";
+std::string pat = fmt::format("./{}.mp4",ObjectName);
+
+int main()
+{
+    VideoCapture video = VideoCapture(pat);
+
+    
+    double fps = video.get(CAP_PROP_FPS);
+    int width = video.get(CAP_PROP_FRAME_WIDTH);
+    int height = video.get(CAP_PROP_FRAME_HEIGHT);
+
+    std::cout<<"FPS"<<fps<<" width"<<width<<" height"<<height<<std::endl;
+
+    Mat frame;
+    int num = video.get(CAP_PROP_FRAME_COUNT);
+    for(int i = 0;i < num-1;i ++)
+    {
+        video.read(frame);
+        imshow("test",frame);
+        imwrite(fmt::format("./{}/{}.jpg",ObjectName,i),frame);
+
+        if(waitKey(30) == 'q') break;
+    }
+
+    // 释放资源
+    destroyWindow("test"); // 销毁窗口
+    video.release(); // 释放对象
+
+    return 0;
+}
---
+++ data/build/CMakeFiles/Video2jpg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Video2jpg.dir/Video2jpg.cpp.o"
+  "Video2jpg"
+  "Video2jpg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Video2jpg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
